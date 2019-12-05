cdc0a023d6c397c70a073c922a1f6f3b32212963
Ilan <ilan.pillemer@gmail.com>
2019-12-05 06:59:06 +0000

    go day 5 part 2

--- go/3/main.go
+++ go/3/main.go
@@ -30,20 +30,15 @@ func main() {
 
 	prog := make([]string, len(orig))
 	copy(prog, orig)
-	//	prog[1] = "12"
-	//	prog[2] = "2"
-
-	//	code := "0"
 	pc := 0
-	input := "1"
+	//part1
+	//	input := "1"
+	//part2
+	input := "5"
 Loop:
 	for {
-		//		fmt.Println("code", code)
 		precode := prog[pc]
 		code, mode1, mode2, _ := modes(precode)
-		//		fmt.Println("len", len(prog))
-		//		fmt.Println("prog", prog)
-		//fmt.Println("code", code, mode1)
 		switch code {
 		case "1", "01": //addition
 			p1 := atoi(prog[pc+1])
@@ -71,15 +66,60 @@ Loop:
 			pc = pc + 2
 		case "4", "04": //output
 			p1 := atoi(prog[pc+1])
-			output := atoi(prog[p1])
+			output := getvalue(mode1, p1, prog)
 			fmt.Println("output => ", output)
 			pc = pc + 2
+		// jumps
+		case "5", "05": //jump if true
+			p1 := atoi(prog[pc+1])
+			op1 := getvalue(mode1, p1, prog)
+			p2 := atoi(prog[pc+2])
+			op2 := getvalue(mode2, p2, prog)
+
+			if op1 != 0 {
+				pc = op2
+				continue
+			}
+			pc = pc + 3
+		case "6", "06": //jump if false
+			p1 := atoi(prog[pc+1])
+			op1 := getvalue(mode1, p1, prog)
+			p2 := atoi(prog[pc+2])
+			op2 := getvalue(mode2, p2, prog)
+
+			if op1 == 0 {
+				pc = op2
+				continue
+			}
+			pc = pc + 3
+		//comparisons
+		case "7", "07": //less than
+			p1 := atoi(prog[pc+1])
+			op1 := getvalue(mode1, p1, prog)
+			p2 := atoi(prog[pc+2])
+			op2 := getvalue(mode2, p2, prog)
+			p3 := atoi(prog[pc+3])
+			if op1 < op2 {
+				prog[p3] = strconv.Itoa(1)
+			} else {
+				prog[p3] = strconv.Itoa(0)
+			}
+			pc = pc + 4
+		case "8", "08": //lequals
+			p1 := atoi(prog[pc+1])
+			op1 := getvalue(mode1, p1, prog)
+			p2 := atoi(prog[pc+2])
+			op2 := getvalue(mode2, p2, prog)
+			p3 := atoi(prog[pc+3])
+			if op1 == op2 {
+				prog[p3] = strconv.Itoa(1)
+			} else {
+				prog[p3] = strconv.Itoa(0)
+			}
+			pc = pc + 4
 		case "99":
-			//	if prog[0] == "19690720" {
 			fmt.Println("HALT!")
-			//				fmt.Printf("noun %v verb %v \n", noun, verb)
 			fmt.Println("POS 0", prog[0])
-			//	}
 			break Loop
 		default:
 			fmt.Println("code", code, mode1)
@@ -95,8 +135,10 @@ func itoa(i int) string {
 	return strconv.Itoa(i)
 }
 func atoi(s string) int {
+	s = strings.TrimSpace(s)
 	i, err := strconv.Atoi(s)
 	if err != nil {
+		fmt.Println("illegal", s, err.Error())
 		panic("oh dear")
 	}
 	return i
@@ -109,7 +151,9 @@ func getvalue(mode int, i int, prog []string) int {
 	return i
 }
 
+// tidy this up before it gets even more complex!!
 func modes(s string) (opcode string, mode1 int, mode2 int, mode3 int) {
+	s = strings.TrimSpace(s)
 	if len(s) == 1 {
 		opcode = s
 		mode1 = 0
@@ -143,6 +187,7 @@ func modes(s string) (opcode string, mode1 int, mode2 int, mode3 int) {
 	}
 
 	if len(s) == 5 {
+		fmt.Println(s)
 		opcode = s[3:]
 		mode1 = atoi(s[2:3])
 		mode2 = atoi(s[1:2])
