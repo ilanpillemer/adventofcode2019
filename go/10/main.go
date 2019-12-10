39c83b6c4a31802dbd92643517071fa820abd4f4
Ilan <ilan.pillemer@gmail.com>
2019-12-10 21:22:50 +0000

    day 10 part2

--- go/10/main.go
+++ go/10/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 )
 
 type P struct {
@@ -31,8 +32,26 @@ func main() {
 	}
 	maxy = y
 	display(grid)
-	displayCounts(grid)
+	//displayCounts(grid)
 	maxVisible(grid)
+	t := targets(P{11, 13}, grid)
+	fmt.Println("#keys ", len(t))
+	keys := make([]int, 0, len(t))
+	for k := range t {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	//sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+	i := 1
+	//145 for test 2
+	for _, k := range keys {
+		fmt.Println(i-160, k, t[k])
+		i++
+	}
+	for _, k := range keys {
+		fmt.Println(i-160, k, t[k])
+		i++
+	}
 
 }
 
@@ -54,6 +73,7 @@ func visible(p P, grid map[P]bool) int {
 				dx := p.x - x
 				dy := p.y - y
 				slope := math.Atan2(float64(dy), float64(dx))
+				//fmt.Println(int(slope * 1E10))
 				slopes[slope] = true
 			}
 		}
@@ -71,7 +91,7 @@ func display(grid map[P]bool) {
 			}
 			print(".")
 		}
-		fmt.Println()
+		fmt.Printf("%2d \n", y)
 	}
 }
 
@@ -106,4 +126,28 @@ func maxVisible(grid map[P]bool) {
 		}
 	}
 	fmt.Printf("%d Max Visible at %v \n", max, pmax)
+}
+
+func targets(p P, grid map[P]bool) map[int][]int {
+	//the number of slopes that contain asteroids are the number of visible asteroids
+	targets := map[int][]int{}
+	for y := 0; y < maxy; y++ {
+		for x := 0; x < maxx; x++ {
+			// in the equation
+			// slope = dx/dy
+			if grid[P{x, y}] && (P{x, y} != p) { // the point contains an asteroid
+				dx := p.x - x
+				dy := p.y - y
+				slope := math.Atan2(float64(dy), float64(dx))
+				key := (int(slope * 1e10))
+				e, ok := targets[key]
+				if !ok {
+					e = []int{}
+				}
+				e = append(e, (x*100)+y)
+				targets[key] = e
+			}
+		}
+	}
+	return targets
 }
