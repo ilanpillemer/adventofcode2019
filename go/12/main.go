4b4844b72d2c82e13ead8be87c82caaf0ca7e241
Ilan <ilan.pillemer@gmail.com>
2019-12-12 14:51:51 +0000

    go day 12 part2

--- go/12/main.go
+++ go/12/main.go
@@ -17,6 +17,33 @@ type P struct {
 	vz int
 }
 
+func itoa(i int) string {
+	return strconv.Itoa(i)
+
+}
+
+func hashx(s []P) string {
+	str := ""
+	for _, c := range s {
+		str = str + itoa(c.x) + "," + itoa(c.vx) + ","
+	}
+	return str
+}
+func hashy(s []P) string {
+	str := ""
+	for _, c := range s {
+		str = str + itoa(c.y) + "," + itoa(c.vy) + ","
+	}
+	return str
+}
+func hashz(s []P) string {
+	str := ""
+	for _, c := range s {
+		str = str + itoa(c.z) + "," + itoa(c.vz) + ","
+	}
+	return str
+}
+
 func main() {
 	in := bufio.NewScanner(os.Stdin)
 
@@ -28,11 +55,38 @@ func main() {
 		state = append(state, P{atoi(m[1]), atoi(m[2]), atoi(m[3]), 0, 0, 0})
 	}
 	fmt.Println(state)
-	for i := 0; i < 1000; i++ {
+	startx := hashx(state)
+	starty := hashy(state)
+	startz := hashz(state)
+
+	var m1, m2, m3 int
+	count := 0
+	tick := 0
+	for count < 3 {
+		tick++
 		state = next(state)
+		nextx := hashx(state)
+		nexty := hashy(state)
+		nextz := hashz(state)
+		if nextx == startx && m1 == 0 {
+			m1 = tick
+			count++
+		}
+		if nexty == starty && m2 == 0 {
+			m2 = tick
+			count++
+		}
+		if nextz == startz && m3 == 0 {
+			m3 = tick
+			count++
+		}
+
 	}
+
 	fmt.Println(state)
-	fmt.Println(total(state))
+	fmt.Println(m1, m2, m3)
+	fmt.Println(LCM(m1, m2, m3))
+	//	fmt.Println(total(state)) part 1 (calculated after a 1000 ticks)
 }
 
 func abs(i int) int {
@@ -145,4 +199,26 @@ func velocity(n []P) []P {
 		n[i] = p
 	}
 	return n
+}
+
+//from https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
